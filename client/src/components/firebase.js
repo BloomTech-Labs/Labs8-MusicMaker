f7abc599686a22a48a186ec3504fc9acacc13969
Keiran <keirankozlowski@gmail.com>
2018-11-06 18:18:11 -0500

    Set up a FirebaseConfig.js React components and imported firebase to the package.json.

---
+++ client/src/components/firebase.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase';
+
+const FirebaseConfig = {
+    apiKey: "AIzaSyCls0XUsqzG0RneHcQfwtmfvoOqHWojHVM",
+    authDomain: "musicmaker-4b2e8.firebaseapp.com",
+    databaseURL: "https://musicmaker-4b2e8.firebaseio.com",
+    projectId: "musicmaker-4b2e8",
+    storageBucket: "musicmaker-4b2e8.appspot.com",
+    messagingSenderId: "849993185408"
+};
+
+firebase.initializeApp(config);
+
+
+export default FirebaseConfig;

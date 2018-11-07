1cf49817d952ea5af2eed2f9d8e7180a674ac8e2
Linh Bouniol <paulalinh89@gmail.com>
2018-11-07 10:34:20 -0800

    Turned unsubmittedVC into a tableVC so that it can be scrollable

--- MusicMaker/MusicMaker/View Controllers/UnsubmittedAssignmentViewController.swift
+++ MusicMaker/MusicMaker/View Controllers/UnsubmittedAssignmentViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class UnsubmittedAssignmentViewController: UIViewController {
+class UnsubmittedAssignmentViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()

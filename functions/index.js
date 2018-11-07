ef914799a20d1ac253eb3e4014be6c567aa60752
JeanetteFernandez <jeanfern555@gmail.com>
2018-11-07 11:43:46 -0600

    Tested GET /testing endpoint: - Successfully worked on postman and browser

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,13 @@
 const functions = require('firebase-functions');
+const express = require('express');
+const app = express();
+
+app.get('/testing', (req, res) => {
+    res.send('Hello World! @' + `${Date.now()}`);
+});
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+
+
+exports.app = functions.https.onRequest(app);

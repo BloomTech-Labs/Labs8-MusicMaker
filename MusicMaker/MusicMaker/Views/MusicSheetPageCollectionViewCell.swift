4b942f674857c0c7e067f01a980b00d198606ab1
Linh Bouniol <paulalinh89@gmail.com>
2018-11-07 13:29:17 -0800

    Added music sheet page collection view cell for showing music sheet

---
+++ MusicMaker/MusicMaker/Views/MusicSheetPageCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  MusicSheetPageCollectionViewCell.swift
+//  MusicMaker
+//
+//  Created by Linh Bouniol on 11/7/18.
+//  Copyright © 2018 Vuk. All rights reserved.
+//
+
+import UIKit
+
+class MusicSheetPageCollectionViewCell: UICollectionViewCell {
+    
+    // MARK: - Outlet
+    
+    @IBOutlet weak var imageView: UIImageView!
+}

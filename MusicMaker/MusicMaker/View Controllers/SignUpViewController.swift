8d70f045ac7c00bb9050c3248e4b018a27926f29
Vuk Radosavljevic <vukrado1@gmail.com>
2018-11-07 14:48:53 -0600

    Added selecting level and instruments options

--- MusicMaker/MusicMaker/View Controllers/SignUpViewController.swift
+++ MusicMaker/MusicMaker/View Controllers/SignUpViewController.swift
@@ -7,24 +7,119 @@
 //
 
 import UIKit
+import Firebase
 
 class SignUpViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    
+    // MARK: - IBOutlets
+    
+    @IBOutlet weak var firstNameTextField: UITextField!
+    @IBOutlet weak var lastNameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var confirmPasswordTextField: UITextField!
+    @IBOutlet weak var levelButton: UIButton!
+    @IBOutlet weak var instrumentButton: UIButton!
+    
+    // MARK: - IBActions
+    
+    @IBAction func selectLevel(_ sender: UIButton) {
+        
+        let beginnerAction = UIAlertAction(title: "Beginner", style: .default) { (action) in
+            sender.setTitle("Beginner", for: .normal)
+        }
+        
+        let intermediateAction = UIAlertAction(title: "Intermediate", style: .default) { (action) in
+            sender.setTitle("Intermediate", for: .normal)
+        }
+        
+        let expertAction = UIAlertAction(title: "Expert", style: .default) { (action) in
+            sender.setTitle("Expert", for: .normal)
+        }
+        
+        let alert = UIAlertController(title: "Select your level of experience", message: "", preferredStyle: .actionSheet)
+        alert.addAction(beginnerAction)
+        alert.addAction(intermediateAction)
+        alert.addAction(expertAction)
+        
+        //Used if its an ipad to present as a popover
+        let popover = alert.popoverPresentationController
+        popover?.permittedArrowDirections = .down
+        popover?.sourceView = sender
+        popover?.sourceRect = sender.bounds
+        
+        present(alert, animated: true, completion: nil)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func selectInstrument(_ sender: UIButton) {
+        let trumpetAction = UIAlertAction(title: "Trumpet", style: .default) { (action) in
+            sender.setTitle("Trumpet", for: .normal)
+        }
+        
+        let fluteAction = UIAlertAction(title: "Flute", style: .default) { (action) in
+            sender.setTitle("Flute", for: .normal)
+        }
+        
+        let violinAction = UIAlertAction(title: "Violin", style: .default) { (action) in
+            sender.setTitle("Violin", for: .normal)
+        }
+        
+        let alert = UIAlertController(title: "Select Your Instrument", message: "", preferredStyle: .actionSheet)
+        alert.addAction(trumpetAction)
+        alert.addAction(fluteAction)
+        alert.addAction(violinAction)
+        
+        //Used if its an ipad to present as a popover
+        let popover = alert.popoverPresentationController
+        popover?.permittedArrowDirections = .down
+        popover?.sourceView = sender
+        popover?.sourceRect = sender.bounds
+        
+        present(alert, animated: true, completion: nil)
     }
-    */
-
+    
+    @IBAction func createAccount(_ sender: Any) {
+        guard let email = emailTextField.text,
+            let password = passwordTextField.text,
+            let firstName = firstNameTextField.text,
+            let lastName = lastNameTextField.text
+        
+        else {return}
+        
+        Auth.auth().createUser(withEmail: email, password: password) { (user, error) in
+            
+            //Error creating user checks different errors and updates UI to let user know why there was an error
+            if error != nil {
+                if let errorCode = AuthErrorCode(rawValue: error!._code) {
+                    switch errorCode {
+                    case .weakPassword:
+                        print("weakPassword")
+                    case .accountExistsWithDifferentCredential:
+                        print("Account already exisits")
+                    case .emailAlreadyInUse:
+                        print("email in use")
+                    case .invalidEmail:
+                        print("invalid email")
+                    case .missingEmail:
+                        print("missing email")
+                    default:
+                        print("error")
+                    }
+                }
+            }
+            
+            let database = Firestore.firestore()
+            
+            if let user = user {
+                let usersUniqueIdentifier = user.user.uid
+                print(usersUniqueIdentifier)
+                database.collection("students").document(usersUniqueIdentifier).collection("settings").addDocument(data: ["email" : email])
+            }
+        }
+    }
+    
+    
+    
+    
 }

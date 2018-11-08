be35bb7575c716b2ff0bbf3db8f7bb393b4b3eb3
Keiran <keirankozlowski@gmail.com>
2018-11-08 01:35:44 -0500

    Set up password reset.

--- client/src/views/landingView.js
+++ client/src/views/landingView.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 // import { connect } from 'react-redux';
 
+import ForgotPW from '../components/ForgotPW';
+
 class LandingPageView extends Component {
 
     render() {
         return(
            <div>
-               <p>Landing Page</p>
+               <h1>Landing Page</h1>
+               <ForgotPW />
            </div> 
         ) 
     }

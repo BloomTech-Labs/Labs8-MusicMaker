1532c61e673ce1f6549fc856361b6a032cf8ab45
Linh Bouniol <paulalinh89@gmail.com>
2018-11-08 10:01:38 -0800

    Implemented record button table view cell

---
+++ MusicMaker/MusicMaker/Views/RecordButtonTableViewCell.swift
@@ -0,0 +1,33 @@
+//
+//  RecordButtonTableViewCell.swift
+//  MusicMaker
+//
+//  Created by Linh Bouniol on 11/8/18.
+//  Copyright © 2018 Vuk. All rights reserved.
+//
+
+import UIKit
+
+class RecordButtonTableViewCell: UITableViewCell {
+    
+    // MARK: - Outlets/Actions
+    
+    @IBOutlet weak var recordButton: UIButton!
+    
+    @IBAction func recordButtonTapped(_ sender: Any) {
+        // present the recording screen
+    }
+    
+    override func awakeFromNib() {  // This will never be called if the cell is created in code.
+        super.awakeFromNib()
+        
+        recordButton.layer.cornerRadius = 2
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

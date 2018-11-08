9a41dcc2abf84a878cc74fdf5cb4c49c24726ca6
Keiran <keirankozlowski@gmail.com>
2018-11-08 00:43:33 -0500

    set up authUser state object that checks for authentication.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 
 import * as routes from '../constants/routes';
+import { firebase } from '../firebase';
 
 import Navigation from './Navigation';
 import LandingPageView from '../views/landingView';
@@ -12,26 +13,39 @@ import DashboardView from '../views/dashboardView';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      authUser: null
+    };
+  }
+
+  componentDidMount() {
+    firebase.auth.onAuthStateChanged(authUser => {
+      authUser
+        ? this.setState({ authUser })
+        : this.setState({ authUser: null });
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <Navigation />
+        <Navigation authUser={ this.state.authUser }/>
 
         <Route
           exact path={ routes.LANDING }
           component={ LandingPageView }
         />
-
         <Route
           exact path={ routes.SIGN_UP }
           component={ SignUpView }
         />
-
         <Route
           exact path={ routes.SIGN_IN }
           component={ SignInView }
         />
-
         <Route
           exact path={ routes.DASHBOARD }
           component={ DashboardView }

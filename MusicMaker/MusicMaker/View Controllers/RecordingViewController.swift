ed055d7549aeaa5c9c3e8eee4993b9989f61954b
Linh Bouniol <paulalinh89@gmail.com>
2018-11-08 15:17:55 -0800

    Created a skeleton recording screen with pdf thumbnails

---
+++ MusicMaker/MusicMaker/View Controllers/RecordingViewController.swift
@@ -0,0 +1,64 @@
+//
+//  RecordingViewController.swift
+//  MusicMaker
+//
+//  Created by Linh Bouniol on 11/8/18.
+//  Copyright © 2018 Vuk. All rights reserved.
+//
+
+import UIKit
+import PDFKit
+
+class RecordingViewController: UIViewController {
+    
+    // MARK: - Properties
+    
+    var pdfDocument: PDFDocument? {
+        didSet {
+            collectionView.reloadData()
+        }
+    }
+
+    // MARK: - Outlet
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    // MARK: - View Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Test
+        pdfDocument = PDFDocument(url: Bundle.main.url(forResource: "SamplePDF", withExtension: "pdf")!)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension RecordingViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return pdfDocument?.pageCount ?? 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MusicSheetPageCell", for: indexPath) as! MusicSheetPageCollectionViewCell
+        
+        cell.pdfView.document = pdfDocument!
+        
+        if let pdfPage = pdfDocument?.page(at: indexPath.item) {
+            cell.pdfView.go(to: pdfPage)
+        }
+        
+        return cell
+    }
+}

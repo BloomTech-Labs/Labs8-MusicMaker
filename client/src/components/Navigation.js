b79d31bc41a1f4654f6de35c294b872ac1237043
Keiran <keirankozlowski@gmail.com>
2018-11-08 00:32:42 -0500

    Set up the ability to sign in and out. Altered nav to render based on if authenticated or not.

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -2,16 +2,28 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import * as routes from '../constants/routes';
+import SignOutButton from '../components/SignOutButton';
 
-const Navigation = () => {
-    return(
-        <div>
-            <Link to={ routes.LANDING }>Landing Page</Link>
-            <Link to={ routes.SIGN_UP }>Sign Up</Link>
-            <Link to={ routes.SIGN_IN }>Sign In</Link>
-            <Link to={ routes.DASHBOARD }>Dashboard</Link>
-        </div>
-    )
-}
+const Navigation = ({ authUser }) => 
+    <div>
+        { authUser 
+            ? <NavigationAuth />
+            : <NavigationNonAuth />
+        }
+    </div>
+
+const NavigationAuth = () => 
+    <div>
+        <Link to={ routes.LANDING }>Landing Page</Link>
+        <Link to={ routes.DASHBOARD }>Dashboard</Link>
+        <SignOutButton />
+    </div>
+
+const NavigationNonAuth = () => 
+    <div>
+        <Link to={ routes.LANDING }>Landing Page</Link>
+        <Link to={ routes.SIGN_UP }>Sign Up</Link>
+        <Link to={ routes.SIGN_IN }>Sign In</Link>
+    </div>
 
 export default Navigation;

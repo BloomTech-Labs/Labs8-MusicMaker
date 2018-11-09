e57d96cded9c60f2a9470459cb633762f64c5e87
JeanetteFernandez <jeanfern555@gmail.com>
2018-11-08 22:01:12 -0600

    Initial commit for working on studentListView.js, this is the structure for a teacher's list of students.

---
+++ client/src/views/studentListView.js
@@ -0,0 +1,16 @@
+import React, { Component} from 'react';
+import { Route, NavLink } from 'react-router-dom';
+
+import SignOutButton from '../components/SignOutButton';
+
+class StudentListView extends Components {
+    state = {
+        students: []
+    }
+
+    render() {
+        return(
+            
+        )
+    }
+}

5aa96717a8dc2c9e478bcbdb62e96a09c2034d19
JeanetteFernandez <jeanfern555@gmail.com>
2018-11-08 22:10:30 -0600

    Added what studentListView.js will render which is a list of students with their names, instruments, and level. I added DashboardNavigation and SideBar commented out because it has not be merged to master yet from the previous PR contianing teacher dashboard skeleton but it's an idea of where it would be placed.

--- client/src/views/studentListView.js
+++ client/src/views/studentListView.js
@@ -10,6 +10,25 @@ class StudentListView extends Components {
 
     render() {
         return(
+            <div className="sudentlist-view">
+                <h1>Students<h1>
+                <nav>
+                    {/* <DashbardNavigation /> */}
+                    <SignOutButton />
+                <nav>
+                {/* <SideBar /> */}
+                <div>
+                    {this.state.students.map(student => 
+                        <div key={sudent.id}>
+                            <NavLink to={`/studentView/${student.id}`}>
+                                <h3>{student.firstName, student.lastName}:</h3>
+                                <h3>{student.instrument}</h3>
+                                <h3>{student.level}</h3>
+                            </NavLink>
+                        </div>
+                    )}
+                </div>
+            </div>
             
         )
     }

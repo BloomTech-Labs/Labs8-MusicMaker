a5cced7b8afdb449be5886f9dbc30e3d63b35677
Keiran <keirankozlowski@gmail.com>
2018-11-14 18:48:05 -0500

    Moved some missing node_modules to the proper location, added oAuth Google requirements to firebaseconfig.

--- client/src/firebase/FirebaseConfig.js
+++ client/src/firebase/FirebaseConfig.js
@@ -7,7 +7,12 @@ const FirebaseConfig = {
     databaseURL: "https://musicmaker-4b2e8.firebaseio.com",
     projectId: "musicmaker-4b2e8",
     storageBucket: "musicmaker-4b2e8.appspot.com",
-    messagingSenderId: "849993185408"
+    messagingSenderId: "849993185408",
+    clientId: "ca6a9322899eb2e3a02f355415650bee2b293ec5.apps.googleusercontent.com",
+    scopes: [
+        "email",
+        "profile",
+    ],
 };
 
 if(!firebase.apps.length) firebase.initializeApp(FirebaseConfig);

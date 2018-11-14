8bbb4b4b52360cda41ff0a21315622b2ea650cd8
Vuk Radosavljevic <vukrado1@gmail.com>
2018-11-14 15:57:23 -0600

    Added showing and hiding of side menu with notifications

--- MusicMaker/MusicMaker/View Controllers/ContainerViewController.swift
+++ MusicMaker/MusicMaker/View Controllers/ContainerViewController.swift
@@ -10,21 +10,37 @@ import UIKit
 
 class ContainerViewController: UIViewController {
 
+    
+    // MARK: - Properties
+    var teachersViewController: TeachersViewController!
+    
+    
+    // MARK: - IBOutlets
+    @IBOutlet weak var sideMenu: UIView!
+    @IBOutlet weak var teachersView: UIView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        let touchGesture = UITapGestureRecognizer(target: self, action: #selector(hideMenu))
+        teachersView.addGestureRecognizer(touchGesture)
+        NotificationCenter.default.addObserver(self, selector: #selector(test), name: NSNotification.Name("test"), object: nil)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc func hideMenu() {
+        UIView.animate(withDuration: 0.4) {
+            self.sideMenu.alpha = 0
+            self.teachersView.backgroundColor = UIColor(red: <#T##CGFloat#>, green: <#T##CGFloat#>, blue: <#T##CGFloat#>, alpha: <#T##CGFloat#>)
+        }
     }
-    */
+    
+    @objc func test() {
+        self.view.bringSubviewToFront(sideMenu)
+        UIView.animate(withDuration: 0.4) {
+            self.sideMenu.alpha = 1
+        }
+    }
 
 }
+
+
--- MusicMaker/MusicMaker/View Controllers/TeachersViewController.swift
+++ MusicMaker/MusicMaker/View Controllers/TeachersViewController.swift
@@ -10,21 +10,14 @@ import UIKit
 
 class TeachersViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+    
+    
+    // MARK: - IBActions
+    @IBAction func showSideMenu(_ sender: Any) {
+        NotificationCenter.default.post(name: NSNotification.Name("test"), object: nil)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
+    
 }

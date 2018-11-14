a386ed578df4297d4606ffe10ff91504c1a5411f
Evan Carlstrom <evan.carlstrom@gmail.com>
2018-11-14 01:55:56 -0500

    added another basic function to return the image data for storage in the database as well as the image itself

--- qrcode.js
+++ qrcode.js
@@ -2,8 +2,19 @@ var QRCode = require('qrcode');
 
 // this is the general QR code creation function, takes a string and returns the code
 
-QRCode.toString('test', function (err, string) {
+let code = QRCode.toString('test', function (err, string) {
   if(err) throw err;
   console.log(string);
   return(string);
-})
+});
+
+// this is the version we'll want to store in the database
+
+let data = QRCode.toDataURL('test', function (err, url) {
+  if(err) throw err;
+  console.log(url);
+  return(url);
+});
+
+return code;
+return data;

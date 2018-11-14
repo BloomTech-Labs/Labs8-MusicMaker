5d7658775b3deb37cb8d639384563a8e61c780ec
Evan Carlstrom <evan.carlstrom@gmail.com>
2018-11-14 02:02:17 -0500

    added (currently commented out) async versions of the QR generation function in case they are better suited to the project

--- qrcode.js
+++ qrcode.js
@@ -1,20 +1,42 @@
 var QRCode = require('qrcode');
 
 // this is the general QR code creation function, takes a string and returns the code
+// using both console logs and returns for testing purposes
 
 let code = QRCode.toString('test', function (err, string) {
   if(err) throw err;
   console.log(string);
-  return(string);
 });
 
-// this is the version we'll want to store in the database
+// this is the version we'll want to store in the database, returns the QR code data itself
 
 let data = QRCode.toDataURL('test', function (err, url) {
   if(err) throw err;
   console.log(url);
-  return(url);
 });
 
 return code;
 return data;
+
+// async versions for reference and implementation if desired
+
+// promise
+
+// let asyncData = QRCode.toDataURL('async test')
+//   .then(url => {
+//     console.log(url)
+//     return(url)
+//   })
+//   .catch(err => {
+//     console.error(err.message)
+//   });
+//
+// // await
+//
+// const asyncAwait = async url => {
+//   try {
+//     console.log(await QRCode.toDataURL(url))
+//   } catch (err) {
+//     console.error(err.message)
+//   }
+// }

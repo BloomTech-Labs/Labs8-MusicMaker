cfb3b057d91b5a85b7729e32a68b43696636da5a
Evan Carlstrom <evan.carlstrom@gmail.com>
2018-11-14 02:13:59 -0500

    added a 16-character random string generator to assist with QR code generation

--- qrcode.js
+++ qrcode.js
@@ -3,14 +3,19 @@ var QRCode = require('qrcode');
 // this is the general QR code creation function, takes a string and returns the code
 // using both console logs and returns for testing purposes
 
-let code = QRCode.toString('test', function (err, string) {
+// adding a random string generator to test a variety of cases rather than a simple 'test' string each time
+// 16 characters for now
+
+let stringGen = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 16);
+
+let code = QRCode.toString(stringGen, function (err, string) {
   if(err) throw err;
   console.log(string);
 });
 
 // this is the version we'll want to store in the database, returns the QR code data itself
 
-let data = QRCode.toDataURL('test', function (err, url) {
+let data = QRCode.toDataURL(stringGen, function (err, url) {
   if(err) throw err;
   console.log(url);
 });

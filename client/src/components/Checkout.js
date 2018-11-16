bc17cb89f94bf0453ce194b44fe421251fa7a3a8
Evan Carlstrom <evan.carlstrom@gmail.com>
2018-11-16 03:06:01 -0500

    commented out code in StripeFrontend.js and Checkout.js to avoid any conflicts for now while other code is worked on.

--- client/src/components/Checkout.js
+++ client/src/components/Checkout.js
@@ -1,41 +1,41 @@
-import React from 'react';
-import axios from 'axios';
-import StripeCheckout from 'react-stripe-checkout';
+// import React from 'react';
+// import axios from 'axios';
+// import StripeCheckout from 'react-stripe-checkout';
 
 // WILL ADD BOTH THESE FILES LATER, COMMENTING THESE OUT UNTIL THEN
 // import STRIPE_PUBLISHABLE from './constants/stripe';
 // import PAYMENT_SERVER_URL from './constants/server';
 
-const CURRENCY = 'USD'; // basic currency is dollars
+// const CURRENCY = 'USD'; // basic currency is dollars
 // no conversion variable is needed
 
-const successPayment = data => {
-  alert('Payment successful!');
-};
-
-const errorPayment = data => {
-  alert('There was an error while processing your payment.');
-};
-
-const onToken = (amount, description) => token =>
-  axios.post(PAYMENT_SERVER_URL,
-    {
-      description,
-      source: token.id,
-      currency: CURRENCY,
-      amount: (amount)
-    })
-    .then(successPayment)
-    .catch(errorPayment);
-
-const Checkout = ({ name, description, amount }) =>
-  <StripeCheckout
-    name = {name}
-    description = {description}
-    amount = {(amount)}
-    token = {onToken(amount, description)}
-    currency = {CURRENCY}
-    stripeKey = {STRIPE_PUBLISHABLE}
-  />
-
-export default Checkout;
+// const successPayment = data => {
+//   alert('Payment successful!');
+// };
+//
+// const errorPayment = data => {
+//   alert('There was an error while processing your payment.');
+// };
+//
+// const onToken = (amount, description) => token =>
+//   axios.post(PAYMENT_SERVER_URL,
+//     {
+//       description,
+//       source: token.id,
+//       currency: CURRENCY,
+//       amount: (amount)
+//     })
+//     .then(successPayment)
+//     .catch(errorPayment);
+//
+// const Checkout = ({ name, description, amount }) =>
+//   <StripeCheckout
+//     name = {name}
+//     description = {description}
+//     amount = {(amount)}
+//     token = {onToken(amount, description)}
+//     currency = {CURRENCY}
+//     stripeKey = {STRIPE_PUBLISHABLE}
+//   />
+//
+// export default Checkout;
--- client/src/components/StripeFrontend.js
+++ client/src/components/StripeFrontend.js
@@ -1,19 +1,21 @@
 // THIS FILE IS A TEST FOR NOW, JUST IMPLEMENTING SOME OF THE FRONT END FUNCTIONALITY FOR STRIPE
 // I WILL COME BACK TO THIS LATER TO GET BOTH ENDS WORKING PROPERLY, THIS IS A TEMPLATE/REFERENCE FOR FRONT-END STRIPE WORK
 
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
 // import Checkout from './Checkout'; will un-comment this when necessary
-import './App.css';
-
-class StripeFrontend extends Component {
-  render() {
-    return (
-      <div className = 'checkout-wrapper'>
-        <Checkout name = {'Test Checkout'}
-                  description = {'Stripe front end test.'}
-                  amount = {1}
-        />
-      </div>
-    );
-  }
-}
+// import './App.css';
+//
+// class StripeFrontend extends Component {
+//   render() {
+//     return (
+//       <div className = 'checkout-wrapper'>
+//         <Checkout name = {'Test Checkout'}
+//                   description = {'Stripe front end test.'}
+//                   amount = {1}
+//         />
+//       </div>
+//     );
+//   }
+// }
+//
+// export default StripeFrontend;

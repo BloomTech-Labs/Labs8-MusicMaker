50daef41e79074b0fdd55f263a58e65a62d77ccb
Evan Carlstrom <evan.carlstrom@gmail.com>
2018-11-16 02:59:41 -0500

    added initial Checkout.js file for Stripe checkout with react-stripe-checkout dependency

---
+++ client/src/components/Checkout.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import axios from 'axios';
+import StripeCheckout from 'react-stripe-checkout';
+
+// WILL ADD BOTH THESE FILES LATER, COMMENTING THESE OUT UNTIL THEN
+// import STRIPE_PUBLISHABLE from './constants/stripe';
+// import PAYMENT_SERVER_URL from './constants/server';

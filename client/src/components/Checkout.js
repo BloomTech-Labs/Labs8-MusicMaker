78feb8342702286248fb155f95494093ff15184a
Evan Carlstrom <evan.carlstrom@gmail.com>
2018-11-16 03:02:53 -0500

    added basic token validation and payment function to checkout.js along with payment success and failure alerts

--- client/src/components/Checkout.js
+++ client/src/components/Checkout.js
@@ -5,3 +5,25 @@ import StripeCheckout from 'react-stripe-checkout';
 // WILL ADD BOTH THESE FILES LATER, COMMENTING THESE OUT UNTIL THEN
 // import STRIPE_PUBLISHABLE from './constants/stripe';
 // import PAYMENT_SERVER_URL from './constants/server';
+
+const CURRENCY = 'USD'; // basic currency is dollars
+// no conversion variable is needed
+
+const successPayment = data => {
+  alert('Payment successful!');
+};
+
+const errorPayment = data => {
+  alert('There was an error while processing your payment.');
+};
+
+const onToken = (amount, description) => token =>
+  axios.post(PAYMENT_SERVER_URL,
+    {
+      description,
+      source: token.id,
+      currency: CURRENCY,
+      amount: (amount)
+    })
+    .then(successPayment)
+    .catch(errorPayment);

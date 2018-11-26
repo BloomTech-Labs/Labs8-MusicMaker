d6b3d05f326386781315f3e68c28868e4d345ed3
Linh Bouniol <paulalinh89@gmail.com>
2018-11-26 14:29:26 -0800

    Started playback view cell

---
+++ MusicMaker/MusicMaker/Views/AssignmentPlaybackTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  AssignmentPlaybackTableViewCell.swift
+//  MusicMaker
+//
+//  Created by Linh Bouniol on 11/26/18.
+//  Copyright © 2018 Vuk. All rights reserved.
+//
+
+import UIKit
+
+class AssignmentPlaybackTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

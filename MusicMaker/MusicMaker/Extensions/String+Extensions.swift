8f24ae805ee696662c6d20f9231b93273aa6eda4
Vuk Radosavljevic <vuk@Vuks-iMac.local>
2018-11-26 00:24:59 -0600

    Added string extension to get initials for a users first and last name

---
+++ MusicMaker/MusicMaker/Extensions/String+Extensions.swift
@@ -0,0 +1,29 @@
+//
+//  String+Extensions.swift
+//  MusicMaker
+//
+//  Created by Vuk Radosavljevic on 11/26/18.
+//  Copyright © 2018 Vuk. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    
+    //Gets the initials for a given first and last name
+    var initials: String {
+        var initials = String()
+        let firstAndLastName = components(separatedBy: .whitespacesAndNewlines)
+        
+        if let firstInitial = firstAndLastName.first?.first {
+            initials.append(firstInitial)
+        }
+        
+        if let lastInitial = firstAndLastName.first?.first {
+            initials.append(lastInitial)
+        }
+        
+        return initials
+    }
+    
+}

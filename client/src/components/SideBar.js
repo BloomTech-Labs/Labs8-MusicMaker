f6dadc1d490370e657855ea79b81de512ad30d54
Keiran <keirankozlowski@gmail.com>
2018-11-26 16:51:42 -0500

    Set up styling for teacher dashboard home.

--- client/src/components/SideBar.js
+++ client/src/components/SideBar.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
- const SideBar = () => {
-    return(
-        <div>
-            <NavLink to="/students">Students</NavLink>
-            <NavLink to="/assignments">Assignments</NavLink>
-            <NavLink to="/billing">Billing</NavLink>
-            <NavLink to="/settings">Settings</NavLink>
-        </div>
-    )
-}
- export default SideBar; 
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+import '../css/index.css';
+
+const SideBar = () => {
+  return (
+    <div className="sidebar">
+      <NavLink to="/students" className="item">Students</NavLink>
+      <NavLink to="/assignments" className="item">Assignments</NavLink>
+      <NavLink to="/billing" className="item">Billing</NavLink>
+      <NavLink to="/settings" className="item">Settings</NavLink>
+    </div>
+  );
+};
+export default SideBar;

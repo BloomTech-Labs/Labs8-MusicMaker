1090223c6e72a607ef966fbb055f45c63cf35b93
Linh Bouniol <paulalinh89@gmail.com>
2018-11-27 21:13:29 -0800

    Added firestore fields to Teacher model
    Co-authored-by: Vuk Radosavljevic <vukrado1@gmail.com>

--- MusicMaker/MusicMaker/Models/Teacher+Convenience.swift
+++ MusicMaker/MusicMaker/Models/Teacher+Convenience.swift
@@ -10,5 +10,23 @@ import Foundation
 import CoreData
 
 extension Teacher {
+    @discardableResult
+    convenience init(firestoreID: String, fields: [String: Any]) {
+        self.init(context: CoreDataStack.shared.mainContext)
+        
+        self.firestoreID = firestoreID
+        update(with: fields)
+    }
     
+    func update(with fields: [String: Any]) {
+        var prefix = ""
+        var lastName = ""
+        
+        if let name = fields["name"] as? [String: Any] {
+            prefix = name["prefix"] as? String ?? ""
+            lastName = name["lastName"] as? String ?? ""
+        }
+        
+        self.name = "\(prefix) \(lastName)"
+    }
 }

8d8b4c6ba16e6308a361785f876c91f1d186d396
Linh Bouniol <paulalinh89@gmail.com>
2018-11-28 11:48:49 -0800

    Added header cell that show the assignment title, due date, instrument and level

--- MusicMaker/MusicMaker/View Controllers/SubmittedAssignmentViewController.swift
+++ MusicMaker/MusicMaker/View Controllers/SubmittedAssignmentViewController.swift
@@ -9,22 +9,42 @@
 import UIKit
 
 class SubmittedAssignmentViewController: UITableViewController {
+    
+    // This is our dummy assignment that is in core data
+    var assignment: Assignment? = MusicMakerModelController.shared.teachers.first?.assignments?.anyObject() as? Assignment
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        tableView.reloadData()
+    }
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return 0
-    }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
+        
+        switch indexPath.row {
+        case 0:
+            let cell = tableView.dequeueReusableCell(withIdentifier: "HeaderCell", for: indexPath) as! AssignmentHeaderTableViewCell
+            
+            //            cell.assignmentTitleLabel.text = "this is a test assignment"
+            //            cell.dueDateLabel.text = "DEC 9"
+            //            cell.dueTimeLabel.text = "8:00 PM"
+            //            cell.instrumentLabel.text = "🎻"
+            
+            cell.assignmentTitle = assignment?.title
+            cell.dueDate = Date()   // sets date and time in custom cell
+            cell.instrument = "🎻"
+            cell.level = .intermediate
+            //            Level(rawValue: assignment?.level)
+            
+            return cell
+        default:
+            fatalError("We forgot a case: \(indexPath.row)")
+        }
     }
 }

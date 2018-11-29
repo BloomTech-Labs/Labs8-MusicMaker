22a8a4dd6564a9290e1a49c5416678794db3e73f
Keiran <keirankozlowski@gmail.com>
2018-11-29 11:53:18 -0500

    Hooked up frontend form for Stripe payment

--- client/src/components/StripeFrontend.js
+++ client/src/components/StripeFrontend.js
@@ -1,21 +1,27 @@
-// THIS FILE IS A TEST FOR NOW, JUST IMPLEMENTING SOME OF THE FRONT END FUNCTIONALITY FOR STRIPE
-// I WILL COME BACK TO THIS LATER TO GET BOTH ENDS WORKING PROPERLY, THIS IS A TEMPLATE/REFERENCE FOR FRONT-END STRIPE WORK
+import React from 'react'
+import StripeCheckout from 'react-stripe-checkout';
 
-// import React, { Component } from 'react';
-// import Checkout from './Checkout'; will un-comment this when necessary
-// import './App.css';
-//
-// class StripeFrontend extends Component {
-//   render() {
-//     return (
-//       <div className = 'checkout-wrapper'>
-//         <Checkout name = {'Test Checkout'}
-//                   description = {'Stripe front end test.'}
-//                   amount = {1}
-//         />
-//       </div>
-//     );
-//   }
-// }
-//
-// export default StripeFrontend;
+export default class TakeMoney extends React.Component {
+  onToken = (token) => {
+    fetch('/save-stripe-token', {
+      method: 'POST',
+      body: JSON.stringify(token),
+    }).then(response => {
+      response.json().then(data => {
+        alert(`We are in business, ${data.email}`);
+      });
+    });
+  }
+
+  // ...
+
+  render() {
+    return (
+      // ...
+      <StripeCheckout
+        token={this.onToken}
+        stripeKey="pk_test_YVKiLW0mvujdoxalKItXwyhj"
+      />
+    )
+  }
+}
--- client/src/views/dashboardBillingView.js
+++ client/src/views/dashboardBillingView.js
@@ -1,9 +1,7 @@
-// general dashboard billing view, will add functionality after Stripe integration
-// assuming there will be a siderbar, so I did not specifically add one in this view.
-
 import React, { Component } from "react";
 
 import Sidebar from "../components/SideBar";
+import TakeMoney from '../components/StripeFrontend';
 
 class DashboardBillingView extends Component {
   render() {
@@ -13,49 +11,7 @@ class DashboardBillingView extends Component {
         <Sidebar />
           <div className="block-container" id="billing">
           <h1 className="subheader">Billing</h1>
-            <div>
-              <input
-                className="bodyText"
-                type="text"
-                placeholder="Card Number"
-                // value and event handlers to follow
-              />
-            </div>
-            <div>
-              <input
-                className="bodyText"
-                type="month"
-                placeholder="Expiration Date"
-                // value and event handlers to follow
-              />
-              <input
-                className="bodyText"
-                type="text"
-                placeholder="CVV"
-                // value and event handlers to follow
-              />
-            </div>
-            <div>
-              <div>
-                <input
-                  className="bodyText"
-                  type="checkbox"
-                  name="option1"
-                  value="monthly"
-                />{" "}
-                1 Month Subscription - $20
-                <br />
-                <input
-                  className="bodyText"
-                  type="checkbox"
-                  name="option2"
-                  value="oneclient"
-                />{" "}
-                1 Client - $1.99
-                <br />
-              </div>
-              <button className="buy-button">Buy Now</button>
-            </div>
+            <TakeMoney />
           </div>
         </div>
       </div> // billing-wrapper

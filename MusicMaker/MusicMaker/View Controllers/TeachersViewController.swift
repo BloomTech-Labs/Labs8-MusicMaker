4f0fb450fb7a167ddb50f4d33b57dfcd9778a06d
Vuk Radosavljevic <vukrado1@gmail.com>
2018-11-28 18:46:20 -0600

    Added table view that fetches teachers and displays them

--- MusicMaker/MusicMaker/View Controllers/TeachersViewController.swift
+++ MusicMaker/MusicMaker/View Controllers/TeachersViewController.swift
@@ -17,13 +17,27 @@ class TeachersViewController: UIViewController {
         super.viewDidLoad()
         let touchGesture = UITapGestureRecognizer(target: self, action: #selector(hideMenuFromUserTap))
         self.view.addGestureRecognizer(touchGesture)
+        MusicMakerModelController.shared.fetchTeachers { (teachers, error) in
+            guard error == nil else {return}
+            if let teachers = teachers {
+                self.teachers = teachers
+                self.tableView.reloadData()
+            }
+        }
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
+    // MARK: - IBOutlets
+    @IBOutlet weak var tableView: UITableView!
     
     // MARK: - Properties
     var sideMenuIsShowing = false
     weak var delegate: TeachersViewControllerDelegate?
     var student: Student?
-    
+    var teachers = [Teacher]()
     // MARK: - Private Methods
     @objc private func hideMenuFromUserTap() {
         if sideMenuIsShowing {
@@ -37,4 +51,22 @@ class TeachersViewController: UIViewController {
         delegate?.menuButtonTapped()
         sideMenuIsShowing = sideMenuIsShowing ? false : true
     }
+}
+
+
+// MARK: - UITableViewDataSource
+extension TeachersViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return teachers.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TeacherCell", for: indexPath)
+        let teacher = teachers[indexPath.row]
+        cell.textLabel?.text = teacher.name
+        cell.imageView?.createInitialsImage(for: teacher.name, backgroundColor: .lightGray)
+        return cell
+    }
+    
+    
 }

1351c4f626915fe9ca45c05007b33e9cd55cf03d
Evan Carlstrom <evan.carlstrom@gmail.com>
2018-11-28 22:36:30 -0500

    completed stripe constants file with export

--- server/stripe_constants.js
+++ server/stripe_constants.js
@@ -1,5 +1,9 @@
-const configureStripe = require('stripe');
-
-const STRIPE_SECRET_KEY = process.env.NODE.ENV === 'production'
-  ? 'live key' // add live key from Stripe API keys dashboard to this field
-  ? 'test key'; // add live key from Stripe API keys dashboard to this field
+// const configureStripe = require('stripe');
+//
+// const STRIPE_SECRET_KEY = process.env.NODE.ENV === 'production'
+//   ? 'live key' // add live key from Stripe API keys dashboard to this field (in .env file)
+//   ? 'test key'; // add live key from Stripe API keys dashboard to this field (in .env file)
+//
+// const stripe = configureStripe(STRIPE_SECRET_KEY);
+//
+// module.exports = stripe;

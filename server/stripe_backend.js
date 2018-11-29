f6e9035e1ef7a41f509f06dadc77436c2a84a6f4
Evan Carlstrom <evan.carlstrom@gmail.com>
2018-11-28 22:40:38 -0500

    added CORS options to stripe backend code

--- server/stripe_backend.js
+++ server/stripe_backend.js
@@ -4,4 +4,11 @@
 const cors = require('cors'); // unnecessary as the main server file already has this
 const bodyParser = require('body-parser');
 
-const CORS_WHITELIST = require('./constants/frontend'); // whichever location the FRONTEND_DEV_URLS and FRONTEND_PROD_URLS are placed
+const CORS_WHITELIST = require('./constants/frontend'); // this is default, use whichever location the FRONTEND_DEV_URLS and FRONTEND_PROD_URLS are placed
+
+const corsOptions = {
+  origin: (origin, callback) =>
+    (CORS_WHITELIST.indexOf(origin) !== -1)
+      ? callback(null, true)
+      : callback(new Error('Operation not allowed by CORS.'))
+};

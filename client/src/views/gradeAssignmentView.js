1eddbf68d78e7af0a136f7b2a19a93d062ed6bd2
Evan Carlstrom <evan.carlstrom@gmail.com>
2018-11-29 22:09:17 -0500

    created a basic component to view graded assignments

---
+++ client/src/views/gradeAssignmentView.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Sidebar from '../components/SideBar';
+import '../css/index.css';
+
+class GradeAssignmentView extends Component {
+  render() {
+    return (
+      <div>Hello.</div>
+    );
+  }
+}
+
+export default GradeAssignmentView;

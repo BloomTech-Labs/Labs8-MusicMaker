5b28aefa7b97a3e3fbf2f44262f6cc16a2f5e6e3
Evan Carlstrom <evan.carlstrom@gmail.com>
2018-11-29 22:26:30 -0500

    commented out gradeAssignmentView code so there are no issues while we work on the endpoint(s) it fetches data from

--- client/src/views/gradeAssignmentView.js
+++ client/src/views/gradeAssignmentView.js
@@ -1,44 +1,44 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import Sidebar from '../components/SideBar';
-import '../css/index.css';
-
-// we can figure out exactly how to navigate to this
-
-class GradeAssignmentView extends Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      title: '',
-      instrument: '',
-      difficulty: ''
-
-      // add pdf and video when we have these endpoints finalized
-    }
-  };
-
-  componentDidMount() {
-    axios
-      .get('https://musicmaker-4b2e8.firebaseapp.com/studentendpointhere)
-      .then(res => {
-        console.log(res.data)
-        this.setState({
-          title: res.data.title,
-          instrument: res.data.instrument,
-          difficulty: res.data.difficulty
-        })
-      })
-      .catch(err => console.error('An error was encountered.', err));
-  } // this will not work until we have the student endpoints complete
-
-  render() {
-    return (
-      <div className = 'flex-container'>
-        <h1>Test</h1>
-      </div>
-    );
-  }
-}
-
-export default GradeAssignmentView;
+// import React, { Component } from 'react';
+// import axios from 'axios';
+// import Sidebar from '../components/SideBar';
+// import '../css/index.css';
+//
+// // we can figure out exactly how to navigate to this
+//
+// class GradeAssignmentView extends Component {
+//
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       title: '',
+//       instrument: '',
+//       difficulty: ''
+//
+//       // add pdf and video when we have these endpoints finalized
+//     }
+//   };
+//
+//   componentDidMount() {
+//     axios
+//       .get('https://musicmaker-4b2e8.firebaseapp.com/studentendpointhere)
+//       .then(res => {
+//         console.log(res.data)
+//         this.setState({
+//           title: res.data.title,
+//           instrument: res.data.instrument,
+//           difficulty: res.data.difficulty
+//         })
+//       })
+//       .catch(err => console.error('An error was encountered.', err));
+//   } // this will not work until we have the student endpoints complete
+//
+//   render() {
+//     return (
+//       <div className = 'flex-container'>
+//         <h1>Test</h1>
+//       </div>
+//     );
+//   }
+// }
+//
+// export default GradeAssignmentView;

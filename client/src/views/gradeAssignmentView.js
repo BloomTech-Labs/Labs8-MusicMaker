6a5bf94165ae98c2c1e7e5579558491f61d17161
Evan Carlstrom <evan.carlstrom@gmail.com>
2018-11-29 22:17:59 -0500

    added constructor and props to gradeAssignmentView

--- client/src/views/gradeAssignmentView.js
+++ client/src/views/gradeAssignmentView.js
@@ -2,10 +2,25 @@ import React, { Component } from 'react';
 import Sidebar from '../components/SideBar';
 import '../css/index.css';
 
+// we can figure out exactly how to navigate to this
+
 class GradeAssignmentView extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: '',
+      instrument: '',
+      difficulty: ''
+
+      // add pdf and video when we have these endpoints finalized
+    }
+  };
+  
   render() {
     return (
-      <div>Hello.</div>
+      <div className = 'flex-container'>
+        <Sidebar />
     );
   }
 }

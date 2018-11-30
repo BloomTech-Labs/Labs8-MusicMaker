b58011f475ad244ab06e25c782934e36fc31788d
Evan Carlstrom <evan.carlstrom@gmail.com>
2018-11-29 22:21:57 -0500

    added componentDidMount() to gradeAssignmentView to set the component's state based on response data from student endpoints

--- client/src/views/gradeAssignmentView.js
+++ client/src/views/gradeAssignmentView.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import Sidebar from '../components/SideBar';
 import '../css/index.css';
 
@@ -16,7 +17,21 @@ class GradeAssignmentView extends Component {
       // add pdf and video when we have these endpoints finalized
     }
   };
-  
+
+  componentDidMount() {
+    axios
+      .get('https://musicmaker-4b2e8.firebaseapp.com/studentendpointhere)
+      .then(res => {
+        console.log(res.data)
+        this.setState({
+          title: res.data.title,
+          instrument: res.data.instrument,
+          difficulty: res.data.difficulty
+        })
+      })
+      .catch(err => console.error('An error was encountered.', err));
+  } // this will not work until we have the student endpoints complete
+
   render() {
     return (
       <div className = 'flex-container'>

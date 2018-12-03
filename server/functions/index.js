d46eea43ed44e6c7ddcbcb9906eed71b915d6598
JeanetteFernandez <jeanfern555@gmail.com>
2018-12-03 11:41:08 -0600

    Initial Commit - In this endpoint the teacher should be able assign a student to an assignment and add the student to the assignment list.

--- server/functions/index.js
+++ server/functions/index.js
@@ -50,9 +50,40 @@ app.get('/', (req, res) => {
     res.status(200).send({MESSAGE: 'HELLO FROM THE BACKEND! :) Visit our Website: https://musicmaker-4b2e8.firebaseapp.com/'});
 });
 
-// GRADED ASSIGNMENT: GET - PUT %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+// ASSIGN STUDENT TO AN ASSIGNMENT & VISE VERSA %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-// Get a completed assignment from a student
+app.post('', (req, res, next) => {
+  try {
+
+  }catch(err){
+    next(err);
+  };
+});
+
+// GRADE ASSIGNMENT: GET - PUT %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%&&&&&&&&&&&&&&&&&&&&%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+// Get a list of students currently assigned to an assignment, ASSIGNED 
+app.get('/teacher/:idTeacher/assignment/:idAssignment/students', (req, res, next) => {
+  try{
+      const teacherId = req.params['idTeacher'];
+      const assignmentId = req.params['idAssignment'];
+      const students = {};  
+
+      const assignedStudentsRef =  db.collection('teachers').doc(teacherId).collection('assignments').doc(assignmentId).collection('students');
+      const allStudents = assignedStudentsRef.get()
+      .then(snap => {
+        snap.forEach(doc => {
+          students[doc.id] = doc.data();
+        })
+        res.status(200).json(students);
+      });
+
+  } catch (err){
+    next (err);
+  }
+});
+
+// Get an assignment from a student
 // FUNCTIONAL AS OF 12/2/18 3 PM
 app.get('/student/:idStudent/teachers/:idTeacher/assignments/:idAssignment', (req, res, next) => {
   try{
@@ -138,7 +169,7 @@ app.get('/teacher/:idTeacher/students/:idStudent', (req, res, next) => {
  }
 });
 
-// Get the list of completed assignments from a student
+// Get the list of assignments from a student
 // FUNCTIONAL AS OF 12/2/18 3 PM
 app.get('/student/:idStudent/teachers/:idTeacher/assignments', (req, res, next) => {
   try{
@@ -161,27 +192,6 @@ app.get('/student/:idStudent/teachers/:idTeacher/assignments', (req, res, next) 
   }
 });
 
-// Get a list of students currently assigned to an assignment, ASSIGNED 
-app.get('/teacher/:idTeacher/assignment/:idAssignment/students', (req, res, next) => {
-  try{
-      const teacherId = req.params['idTeacher'];
-      const assignmentId = req.params['idAssignment'];
-      const students = {};  
-
-      const assignedStudentsRef =  db.collection('teachers').doc(teacherId).collection('assignments').doc(assignmentId).collection('students');
-      const allStudents = assignedStudentsRef.get()
-      .then(snap => {
-        snap.forEach(doc => {
-          students[doc.id] = doc.data();
-        })
-        res.status(200).json(students);
-      });
-
-  } catch (err){
-    next (err);
-
-  }
-});
 
 // UNGRADED ASSIGNMENTS : POST - GET (All & Single Ungraded Assignment) %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 

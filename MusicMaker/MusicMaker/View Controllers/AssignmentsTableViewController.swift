b503bcc084372d950b773638fa4c91c609ca19e1
Linh Bouniol <paulalinh89@gmail.com>
2018-12-05 11:06:37 -0800

    Added assignments tableVC

--- MusicMaker/MusicMaker/View Controllers/AssignmentsTableViewController.swift
+++ MusicMaker/MusicMaker/View Controllers/AssignmentsTableViewController.swift
@@ -9,82 +9,66 @@
 import UIKit
 
 class AssignmentsTableViewController: UITableViewController {
+    
+    // MARK: - Properties
+    
+    var assignments: [Assignment] = []
+    var teacher: Teacher?
 
+    // MARK: - View Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        if let teacher = teacher {
+            self.assignments = teacher.sortedAssignments
+            self.tableView.reloadData()
+        }
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return assignments.count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "AssignmentCell", for: indexPath) as! AssignmentTableViewCell
 
-        // Configure the cell...
+        cell.assignment = assignments[indexPath.row]
 
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let assignment = assignments[indexPath.row]
+        
+        switch assignment.status {
+        case .unsubmitted(isLate: _, isInProgress: _):
+            performSegue(withIdentifier: "ShowUnsubmittedAssignment", sender: self)
+        case .submitted(grade: _):
+            performSegue(withIdentifier: "ShowSubmittedAssignment", sender: self)
+        }
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
+    
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        guard let indexPath = tableView.indexPathForSelectedRow else { return }
+        let assignment = assignments[indexPath.row]
+        
+        if let destinationVC = segue.destination as? UnsubmittedAssignmentViewController {
+            destinationVC.assignment = assignment
+        } else if let destinationVC = segue.destination as? SubmittedAssignmentViewController {
+            destinationVC.assignment = assignment
+        }
     }
-    */
-
 }

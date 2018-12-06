b3f2fb8daececaff3406e91ab26bbaad7bd35e7f
Keiran <keirankozlowski@gmail.com>
2018-12-05 19:14:02 -0500

    Completed basic styling.

--- client/src/views/createAssignmentView.js
+++ client/src/views/createAssignmentView.js
@@ -1,51 +1,48 @@
 import React, { Component } from "react";
+import { Input, Form, FormGroup, Label, Button } from 'reactstrap';
 
 import Sidebar from "../components/SideBar";
 
 class CreateAssignmentView extends Component {
   render() {
     return (
-      <div className="container">
-        <div className="flex-container">
-          <Sidebar />
-          <div className="block-container">
-            <form className="assignmentForm" type="submit">
-              <div>
-                <input
-                  className="subheader"
-                  type="text"
-                  name="assignment-name"
-                  placeholder="Assignment Name"
-                />
-              </div>
-              <div>
-                <input
-                  className="bodyText"
-                  type="text"
-                  name="piece"
-                  placeholder="Piece"
-                />
-                <select className="bodyText" name="instrument">
-                  <option value="guitar">Guitar</option>
-                  <option value="piano">Piano</option>
-                  <option value="trumpet">Trumpet</option>
-                  <option value="violin">Violin</option>
-                  <option value="saxophone">Saxophone</option>
-                  <option value="drum">Drum</option>
-                </select>
-                <select className="bodyText" name="level">
-                  <option value="beginner">Beginner</option>
-                  <option value="intermediate">Intermediate</option>
-                  <option value="advanced">Advanced</option>
-                </select>
-              </div>
-              <div>
-                <label className="bodyText">Instructions</label>
-                <input className="bodyText" type="text" name="instructions" />
-              </div>
-              <button className="submit">Submit</button>
-            </form>
-          </div>
+      <div>
+        <Sidebar />
+        <div>
+          <Form>
+            <FormGroup>
+              <Input 
+                type="text"
+                name="assignment-name"
+                placeholder="Assignment Name"
+              />
+            </FormGroup>
+            <FormGroup inline>
+              <Input  type="text" name="piece" placeholder="Piece Name" />
+
+              <Label>Instrument</Label>
+              <Input type="select" name="instrument">
+                <option value="guitar">Guitar</option>
+                <option value="piano">Piano</option>
+                <option value="trumpet">Trumpet</option>
+                <option value="violin">Violin</option>
+                <option value="saxophone">Saxophone</option>
+                <option value="drum">Drum</option>
+              </Input>
+
+              <Label>Experience Level</Label>
+              <Input type="select"  name="level">
+                <option value="beginner">Beginner</option>
+                <option value="intermediate">Intermediate</option>
+                <option value="advanced">Advanced</option>
+              </Input>
+            </FormGroup>
+            <FormGroup>
+              <Label>Instructions</Label>
+              <Input  type="textarea" name="instructions" placeholder="Instructions go here..." />
+            </FormGroup>
+            <Button className="submit">Submit</Button>
+          </Form>
         </div>
       </div>
     );

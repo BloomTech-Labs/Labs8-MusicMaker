9f31560791cf4bfc447fe19dd698cae6f6427ecf
Keiran <keirankozlowski@gmail.com>
2018-12-05 19:28:18 -0500

    Removed default styling

--- client/src/views/dashboardAssignmentsView.js
+++ client/src/views/dashboardAssignmentsView.js
@@ -35,23 +35,21 @@ class DashboardAssignmentsView extends Component {
     }
   render() {
     return (
-      <div className="container">
-        <div className="flex-container">
+      <div>
           <Sidebar />
-          <div className="block-container" id="assignments">
-            <h1 className="subheader">Assignments View</h1>
-            <h5 className="subheader">Test: This is an ungraded assignment for a specific teacher</h5>
-            <p className="bodyText">Assignment Name: {this.state.assignmentName}</p>
-            <p className="bodyText">Piece: {this.state.piece}</p>
-            <p className="bodyText">Instrument: {this.state.instrument}</p>
-            <p className="bodyText">Level: {this.state.level}</p>
-            <p className="bodyText">Sheet Music: <a href={this.state.sheetMusic}><img src={this.state.sheetMusic} alt="pdf image" height="42" width="42"/></a></p>
-            <p className="bodyText">Instructions:{this.state.instructions}</p>
+          <div>
+            <h1>Assignments View</h1>
+            <h5>Test: This is an ungraded assignment for a specific teacher</h5>
+            <p>Assignment Name: {this.state.assignmentName}</p>
+            <p>Piece: {this.state.piece}</p>
+            <p>Instrument: {this.state.instrument}</p>
+            <p>Level: {this.state.level}</p>
+            <p>Sheet Music: <a href={this.state.sheetMusic}><img src={this.state.sheetMusic} alt="pdf image" height="42" width="42"/></a></p>
+            <p>Instructions:{this.state.instructions}</p>
           </div>
-            <div className="block-container" id="assignments">
-              <NavLink to='/assignments/create' className="bodyText"> + </NavLink>
+            <div>
+              <NavLink to='/assignments/create'><h1> + </h1></NavLink>
             </div>
-        </div>
       </div>
     );
   }

ab6971f2dcb2ba7b0171a1098195476a74c79d10
Vuk Radosavljevic <vukrado1@gmail.com>
2018-12-07 14:10:45 -0600

    Added constraints for first container view for signup process

---
+++ MusicMaker/MusicMaker/View Controllers/AddTeacherOptionsViewController 2.swift
@@ -0,0 +1,183 @@
+//
+//  QRScannerViewController.swift
+//  MusicMaker
+//
+//  Created by Vuk Radosavljevic on 11/30/18.
+//  Copyright © 2018 Vuk. All rights reserved.
+//
+
+import UIKit
+import FirebaseAuth
+import FirebaseFirestore
+
+class AddTeacherOptionsViewController: UIViewController {
+
+    // MARK: - View Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupNavigationBar()
+        
+        setupScrollViewChildren()
+        scrollView.isPagingEnabled = true
+        
+
+    }
+    
+    private func setupScrollViewChildren() {
+        qrScanner = storyboard!.instantiateViewController(withIdentifier: "QRScanner") as? QRScannerViewController
+        qrScanner.delegate = self
+        scrollView.addSubview(qrScanner.view)
+        addChild(qrScanner)
+        qrScanner.didMove(toParent: self)
+        qrScanner.view.translatesAutoresizingMaskIntoConstraints = false
+        let qrReader = storyboard!.instantiateViewController(withIdentifier: "QRReader") as! AddQRPhotoViewController
+        qrReader.delegate = self
+        
+        scrollView.addSubview(qrReader.view)
+        addChild(qrReader)
+        qrReader.view.translatesAutoresizingMaskIntoConstraints = false
+        qrReader.didMove(toParent: self)
+        
+        let views: [String: UIView] = ["view": scrollView, "qrScanner": qrScanner.view, "qrReader": qrReader.view]
+        
+        let verticalConstraints = NSLayoutConstraint.constraints(withVisualFormat: "V:|[qrScanner(==view)]|", options: [.alignAllLeft, .alignAllRight], metrics: nil, views: views)
+        
+        let horizontalConstraints = NSLayoutConstraint.constraints(withVisualFormat: "H:|[qrScanner(==view)][qrReader(==view)]|", options: [.alignAllTop, .alignAllBottom], metrics: nil, views: views)
+        
+        NSLayoutConstraint.activate(verticalConstraints + horizontalConstraints)
+        
+    }
+    override func viewDidLayoutSubviews() {
+        
+        preferredContentSize = view.systemLayoutSizeFitting(UIView.layoutFittingCompressedSize)
+    }
+
+    // MARK: - IBOutlets
+    
+    @IBOutlet weak var underlineBarLeadingConstraint: NSLayoutConstraint!
+    @IBOutlet weak var underlineBar: UIView! {
+        didSet {
+//            underlineBar.layer.borderColor = UIColor.blue3.cgColor
+//            underlineBar.layer.borderWidth = 1
+        }
+    }
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    // MARK: - Properties
+    private var isScrolling = true
+    var isSigningUpWithGoogle = false
+    var teacherUniqueId: String?
+    var email: String?
+    var qrScanner: QRScannerViewController!
+    
+    // MARK: - Private Methods
+    
+    private func setupNavigationBar() {
+        self.navigationController?.navigationBar.isHidden = false
+        title = nil
+        self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
+        self.navigationController?.navigationBar.shadowImage = UIImage()
+    }
+    
+    // MARK: - IBActions
+
+    @IBAction func addQrCodeFromPhotos(_ sender: Any) {
+        underlineBarLeadingConstraint.constant = view.frame.width / 2
+        UIView.animate(withDuration: 0.75, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+            self.view.layoutIfNeeded()
+        }, completion: nil)
+        
+        var frame = scrollView.frame
+        frame.origin.x = frame.size.width * 1
+        frame.origin.y = 0
+        isScrolling = false
+        scrollView.scrollRectToVisible(frame, animated: true)
+        
+        
+    }
+    
+    
+    @IBAction func scanQrCodeButtonTapped(_ sender: Any) {
+        underlineBarLeadingConstraint.constant = 0
+        UIView.animate(withDuration: 0.75, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+            self.view.layoutIfNeeded()
+        }, completion: nil)
+        isScrolling = false
+        scrollView.scrollRectToVisible(scrollView.frame, animated: true)
+        
+    }
+    
+    // MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        switch segue.identifier {
+        case "ShowSignUp":
+            
+            if let navigationController = segue.destination as? UINavigationController,
+                let studentSignupVC = navigationController.topViewController as? StudentSignupViewController {
+                studentSignupVC.isSigningUpWithGoogle = isSigningUpWithGoogle
+                studentSignupVC.teacherUniqueId = teacherUniqueId
+                studentSignupVC.email = email
+            }
+        default:
+            break
+        }
+    }
+    
+}
+
+// MARK: - UIScrollViewDelegate
+extension AddTeacherOptionsViewController: UIScrollViewDelegate {
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        if isScrolling {
+            underlineBarLeadingConstraint.constant = scrollView.contentOffset.x / 2
+        }
+        
+    }
+    
+    func scrollViewDidEndScrollingAnimation(_ scrollView: UIScrollView) {
+        isScrolling = true
+    }
+}
+
+// MARK: - QRScanningDelegate
+extension AddTeacherOptionsViewController: QRScanning {
+    
+    func qrCodeScanned(_ qrCode: String) {
+        let currentUser = Auth.auth().currentUser
+        if let user = currentUser {
+            let database = Firestore.firestore()
+            
+            
+            let teacherDocument = database.collection("teachers").document(qrCode)
+            
+            teacherDocument.getDocument { (documentSnapshot, error) in
+                if let error = error {
+                    NSLog("Error loading teacher \(error)")
+                    return
+                }
+                if let name = documentSnapshot?.data()?["name"] as? [String : String] {
+                    database.collection("students").document(user.uid).collection("teachers").document(qrCode).setData(["exists": true, "name": name])
+                    NotificationCenter.default.post(name: .newTeacher, object: nil)
+                }
+            }
+            
+//            qrScanner.qrView.captureSession?.startRunning()
+        } else {
+            teacherUniqueId = qrCode
+            self.performSegue(withIdentifier: "ShowSignUp", sender: nil)
+        }
+    }
+    
+    
+   
+    
+}
+
+extension Notification.Name {
+    static let newTeacher = Notification.Name("New Teacher")
+    static let qrHidden = Notification.Name("QR Hidden")
+    static let qrShown = Notification.Name("QR Shown")
+}
+
+
---
+++ MusicMaker/MusicMaker/View Controllers/InitialViewController 2.swift
@@ -0,0 +1,194 @@
+//
+//  InitialViewController.swift
+//  MusicMaker
+//
+//  Created by Vuk Radosavljevic on 11/27/18.
+//  Copyright © 2018 Vuk. All rights reserved.
+//
+
+import UIKit
+import GoogleSignIn
+import FirebaseAuth
+import FirebaseFirestore
+
+class InitialViewController: UIViewController, GIDSignInUIDelegate {
+    
+    // MARK: - Properties
+    let database = Firestore.firestore()
+    let blurredBackgroundView = UIVisualEffectView()
+    
+    //Used for google authentication to pass email so it can post it to firestore
+    var isSigningUpWithGoogle = false
+    var email: String?
+    
+    
+    // MARK: - View Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("VIEW DID LOAD - \(loginButton.frame.height)")
+        //Used for the blurred view
+        self.definesPresentationContext = true
+        self.providesPresentationContextTransitionStyle = true
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.navigationController?.navigationBar.isHidden = true
+        print("VIEW WILL APPEAR - \(loginButton.frame.height)")
+    }
+    
+    
+    
+    
+    // MARK: - IBOutlets
+    @IBOutlet weak var loginButton: UIButton! {
+        didSet {
+            print("DID SET - \(loginButton.frame.height)")
+        }
+    }
+    
+    
+    @IBOutlet weak var signupButton: UIButton!
+    
+    // MARK: - Overrides
+ 
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        print("VIEW DID LAYOUT - \(loginButton.frame.height)")
+
+        signupButton.layer.cornerRadius = signupButton.frame.height / 2
+        loginButton.layer.cornerRadius = loginButton.frame.height / 2
+        blurredBackgroundView.frame = view.frame
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        print("VIEW WILL LAYOUT - \(loginButton.frame.height)")
+
+        signupButton.layer.cornerRadius = signupButton.frame.height / 2
+        loginButton.layer.cornerRadius = loginButton.frame.height / 2
+        blurredBackgroundView.frame = view.frame
+    }
+    
+    // MARK: - IBActions
+    @IBAction func showLoginOptions(_ sender: Any) {
+        animateAdditionOfABlurredBackground()
+    }
+    @IBAction func showSignupOptions(_ sender: Any) {
+        
+        animateAdditionOfABlurredBackground()
+    }
+    
+    
+    
+    
+    // MARK: - Private Methods
+    private func animateAdditionOfABlurredBackground() {
+        
+        blurredBackgroundView.frame = view.frame
+        self.view.addSubview(self.blurredBackgroundView)
+        UIView.animate(withDuration: 0.5, animations: {
+            self.blurredBackgroundView.effect = UIBlurEffect(style: .light)
+        })
+    }
+    
+    private func animateRemovalOfBlurredBackground(with duration: TimeInterval) {
+        UIView.animate(withDuration: duration, animations: {
+            self.blurredBackgroundView.effect = nil
+        }) { (_) in
+            self.blurredBackgroundView.removeFromSuperview()
+        }
+    }
+    
+    // MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        switch segue.identifier {
+        
+        case "ShowLoginOptions":
+            if let authenticationOptionsVC = segue.destination as? AuthenticationOptionsViewController {
+                authenticationOptionsVC.delegate = self
+                authenticationOptionsVC.modalPresentationStyle = .overFullScreen
+                authenticationOptionsVC.newUser = false
+            }
+        case "ShowSignupOptions":
+            if let authenticationOptionsVC = segue.destination as? AuthenticationOptionsViewController {
+                authenticationOptionsVC.delegate = self
+                authenticationOptionsVC.modalPresentationStyle = .overFullScreen
+                authenticationOptionsVC.newUser = true
+            }
+        case "ShowSignupScreen":
+            if let destinationVC = segue.destination as? AddTeacherOptionsViewController {
+                destinationVC.isSigningUpWithGoogle = isSigningUpWithGoogle
+            }
+        case "SignUpWithGoogle":
+            if let destinationVC = segue.destination as? AddTeacherOptionsViewController {
+                destinationVC.email = email
+                destinationVC.isSigningUpWithGoogle = isSigningUpWithGoogle
+            }
+        default:
+            ()
+        }
+    }
+    
+    
+}
+
+// MARK: - GIDSignInDelegat
+extension InitialViewController: GIDSignInDelegate {
+    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
+        
+        if let error = error {
+            NSLog("Error signing in with google : \(error)")
+            return
+        }
+        
+        guard let authentication = user.authentication else { return }
+        let credential = GoogleAuthProvider.credential(withIDToken: authentication.idToken, accessToken: authentication.accessToken)
+        
+        Auth.auth().signInAndRetrieveData(with: credential) { (authResult, error) in
+            if let error = error {
+                print(error)
+                return
+            }
+            let currentUser = Auth.auth().currentUser
+            let currentUserReference = self.database.collection("students").document(currentUser?.uid ?? "")
+            currentUserReference.getDocument(completion: { (document, error) in
+                if let document = document, document.exists {
+                    self.performSegue(withIdentifier: "ShowTeachers", sender: nil)
+                } else {
+                    if let email = currentUser?.email {
+                        self.email = email
+                    }
+                    self.isSigningUpWithGoogle = true
+                    self.performSegue(withIdentifier: "SignUpWithGoogle", sender: nil)
+                }
+            })
+        }
+    }
+    
+  
+}
+
+// MARK: - AuthenticationOptionsViewControllerDelegate
+extension InitialViewController: AuthenticationOptionsViewControllerDelegate {
+    
+    func authenticateWithEmail(for newUser: Bool) {
+        animateRemovalOfBlurredBackground(with: 0.3)
+        self.performSegue(withIdentifier: newUser ? "ShowSignupScreen" : "ShowLoginScreen", sender: nil)
+    }
+    
+    
+    
+    func dismissOptions() {
+        animateRemovalOfBlurredBackground(with: 0.5)
+    }
+    
+    func authenticateWithGoogle() {
+        animateRemovalOfBlurredBackground(with: 0.3)
+        GIDSignIn.sharedInstance().delegate=self
+        GIDSignIn.sharedInstance().uiDelegate=self
+        GIDSignIn.sharedInstance()?.signIn()
+    }
+    
+    
+}

294dd20dea3b14815f7ca33099b350c375383c97
Evan Carlstrom <evan.carlstrom@gmail.com>
2018-12-09 18:09:20 -0500

    set up basic Reactstrap layout for landing page with title, subtitle, text formatting, button, and header area with image

--- client/src/views/landingView.js
+++ client/src/views/landingView.js
@@ -10,30 +10,30 @@ class LandingPageView extends Component {
   render() {
     return (
       <div className="landing" style={formContainer}>
-        <img src={jumbotron} width="100%" style={{ padding: "30px" }} />
+        <CardImg src={jumbotron} width="100%" style={{ padding: "30px" }} />
         <Card>
           <CardBody>
-            <h1 className="subheader">Landing Page</h1>
-            <button className="buyNow_Button"> BUY NOW </button>
-            <p className="bodyText">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed commodo
-              consequat nisl in ornare. Nulla quis fringilla ipsum, blandit luctus
-              purus. Phasellus suscipit enim massa, id molestie magna consectetur
-              ac. Praesent ut posuere tortor. Phasellus aliquam risus elit, commodo
-              accumsan arcu cursus et. Pellentesque ac pharetra eros, ut facilisis
-              arcu. Mauris hendrerit nibh mi, non fermentum quam pharetra non. Etiam
-              eu felis ac tortor accumsan finibus quis in odio. Suspendisse potenti.
-            </p>
-            <p className="bodyText">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed commodo
-              consequat nisl in ornare. Nulla quis fringilla ipsum, blandit luctus
-              purus. Phasellus suscipit enim massa, id molestie magna consectetur
-              ac. Praesent ut posuere tortor. Phasellus aliquam risus elit, commodo
-              accumsan arcu cursus et. Pellentesque ac pharetra eros, ut facilisis
-              arcu. Mauris hendrerit nibh mi, non fermentum quam pharetra non. Etiam
-              eu felis ac tortor accumsan finibus quis in odio. Suspendisse potenti.
-            </p>
-            <button className="buyNow_Button"> BUY NOW </button>
+            <CardTitle>Landing Page</CardTitle>
+              <CardSubtitle>Placeholder</CardSubtitle>
+                <CardText className="bodyText">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed commodo
+                  consequat nisl in ornare. Nulla quis fringilla ipsum, blandit luctus
+                  purus. Phasellus suscipit enim massa, id molestie magna consectetur
+                  ac. Praesent ut posuere tortor. Phasellus aliquam risus elit, commodo
+                  accumsan arcu cursus et. Pellentesque ac pharetra eros, ut facilisis
+                  arcu. Mauris hendrerit nibh mi, non fermentum quam pharetra non. Etiam
+                  eu felis ac tortor accumsan finibus quis in odio. Suspendisse potenti.
+                </CardText>
+                <CardText className="bodyText">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed commodo
+                  consequat nisl in ornare. Nulla quis fringilla ipsum, blandit luctus
+                  purus. Phasellus suscipit enim massa, id molestie magna consectetur
+                  ac. Praesent ut posuere tortor. Phasellus aliquam risus elit, commodo
+                  accumsan arcu cursus et. Pellentesque ac pharetra eros, ut facilisis
+                  arcu. Mauris hendrerit nibh mi, non fermentum quam pharetra non. Etiam
+                  eu felis ac tortor accumsan finibus quis in odio. Suspendisse potenti.
+                </CardText>
+              <Button className="buyNow_Button"> BUY NOW </Button>
           </CardBody>
         </Card>
       </div>

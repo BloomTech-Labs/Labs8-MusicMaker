8d5ce294380e7b9000795ed4b052399a01dfd56b
Evan Carlstrom <evan.carlstrom@gmail.com>
2018-12-09 17:48:54 -0500

    created basic card structure for landing page design, adjusted image to give the header area some whitespace

--- client/src/views/landingView.js
+++ client/src/views/landingView.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 import jumbotron from '../less/imgs/landingView.png';
 
-import { Card, CardBody, CardImg, CardSubtitle, CardText, CardTitle, Button } from 'reactstrap';
+import { Button, Card, CardBody, CardImg, CardSubtitle, CardText, CardTitle } from 'reactstrap';
 
 const formContainer = { maxWidth: 800, margin: '0 auto 10px', border: "3px solid #A9E8DC"}
 
@@ -10,28 +10,32 @@ class LandingPageView extends Component {
   render() {
     return (
       <div className="landing" style={formContainer}>
-        <img src={jumbotron} width="100%" />
-        <h1 className="subheader">Landing Page</h1>
-        <button className="buyNow_Button"> BUY NOW </button>
-        <p className="bodyText">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed commodo
-          consequat nisl in ornare. Nulla quis fringilla ipsum, blandit luctus
-          purus. Phasellus suscipit enim massa, id molestie magna consectetur
-          ac. Praesent ut posuere tortor. Phasellus aliquam risus elit, commodo
-          accumsan arcu cursus et. Pellentesque ac pharetra eros, ut facilisis
-          arcu. Mauris hendrerit nibh mi, non fermentum quam pharetra non. Etiam
-          eu felis ac tortor accumsan finibus quis in odio. Suspendisse potenti.
-        </p>
-        <p className="bodyText">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed commodo
-          consequat nisl in ornare. Nulla quis fringilla ipsum, blandit luctus
-          purus. Phasellus suscipit enim massa, id molestie magna consectetur
-          ac. Praesent ut posuere tortor. Phasellus aliquam risus elit, commodo
-          accumsan arcu cursus et. Pellentesque ac pharetra eros, ut facilisis
-          arcu. Mauris hendrerit nibh mi, non fermentum quam pharetra non. Etiam
-          eu felis ac tortor accumsan finibus quis in odio. Suspendisse potenti.
-        </p>
-        <button className="buyNow_Button"> BUY NOW </button>
+        <img src={jumbotron} width="100%" style={{ padding: "30px" }} />
+        <Card>
+          <CardBody>
+            <h1 className="subheader">Landing Page</h1>
+            <button className="buyNow_Button"> BUY NOW </button>
+            <p className="bodyText">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed commodo
+              consequat nisl in ornare. Nulla quis fringilla ipsum, blandit luctus
+              purus. Phasellus suscipit enim massa, id molestie magna consectetur
+              ac. Praesent ut posuere tortor. Phasellus aliquam risus elit, commodo
+              accumsan arcu cursus et. Pellentesque ac pharetra eros, ut facilisis
+              arcu. Mauris hendrerit nibh mi, non fermentum quam pharetra non. Etiam
+              eu felis ac tortor accumsan finibus quis in odio. Suspendisse potenti.
+            </p>
+            <p className="bodyText">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed commodo
+              consequat nisl in ornare. Nulla quis fringilla ipsum, blandit luctus
+              purus. Phasellus suscipit enim massa, id molestie magna consectetur
+              ac. Praesent ut posuere tortor. Phasellus aliquam risus elit, commodo
+              accumsan arcu cursus et. Pellentesque ac pharetra eros, ut facilisis
+              arcu. Mauris hendrerit nibh mi, non fermentum quam pharetra non. Etiam
+              eu felis ac tortor accumsan finibus quis in odio. Suspendisse potenti.
+            </p>
+            <button className="buyNow_Button"> BUY NOW </button>
+          </CardBody>
+        </Card>
       </div>
     );
   }

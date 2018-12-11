6ba973a2f2b3fab67758460279bc8bfefa77f7ef
Vuk Radosavljevic <vukrado1@gmail.com>
2018-12-11 16:17:56 -0600

    Added star ratings and instrument background selection

--- MusicMaker/MusicMaker/View Controllers/LevelAndInstrumentViewController.swift
+++ MusicMaker/MusicMaker/View Controllers/LevelAndInstrumentViewController.swift
@@ -12,11 +12,10 @@ import Lottie
 
 class LevelAndInstrumentViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        // Do any additional setup after loading the view.
-    }
+ 
+    // MARK: - Properties
+    private let allInstrument = ["Guitar", "Piano", "Drums", "Saxophone", "Trumpet", "Violin"]
+    private var studentsInstrument: String?
     
     // MARK: - Delegate
     weak var delegate: LevelAndInstrumentViewControllerDelegate?
@@ -28,29 +27,54 @@ class LevelAndInstrumentViewController: UIViewController {
         }
     }
     
-    @IBOutlet weak var starRating: StarRating!
     @IBOutlet weak var levelLabel: UILabel!
+    @IBOutlet weak var beginnerStar: UIButton!
+    @IBOutlet weak var intermediateStar: UIButton!
+    @IBOutlet weak var expertStar: UIButton!
 
-
+    
+    
+    @IBOutlet var instrumentButtons: [UIButton]!
+    
     
     // MARK: - IBActions
-    
-    @IBAction func starRatingChanged(_ sender: StarRating) {
-        switch sender.value {
-        case 1:
+    @IBAction func levelChanged(_ sender: UIButton) {
+        switch sender.tag {
+        case 0:
+            intermediateStar.titleLabel?.textColor = UIColor.blue4
+            expertStar.titleLabel?.textColor = UIColor.blue4
             levelLabel.text = "Beginner"
-        case 2:
+        case 1:
+            intermediateStar.setTitleColor(UIColor.blue2, for: .normal)
+            expertStar.titleLabel?.textColor = UIColor.blue4
             levelLabel.text = "Intermediate"
-        case 3:
+        case 2:
+            intermediateStar.titleLabel?.textColor = UIColor.blue2
+            expertStar.setTitleColor(UIColor.blue2, for: .normal)
             levelLabel.text = "Expert"
         default:
             break
         }
     }
     
+    @IBAction func instrumentSelected(_ sender: UIButton) {
+        for instrument in instrumentButtons {
+            if instrument.tag == sender.tag {
+                instrument.backgroundColor = UIColor.lightGray
+            } else {
+                instrument.backgroundColor = UIColor.clear
+            }
+        }
+        studentsInstrument = allInstrument[sender.tag]
+        signupButton.alpha = 1
+        signupButton.isEnabled = true
+    }
+    
     @IBAction func signUp(_ sender: Any) {
-        if let instrument = instrumentTextField.text {
+        if let instrument = studentsInstrument {
             delegate?.signUpButtonTapped(with: levelLabel.text!, instrument: instrument)
+        } else {
+            presentInformationalAlertController(title: "No instrument is selected", message: "Please select an instrument to complete sign up")
         }
     }
 }

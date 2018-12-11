460e4dd3f18b1999ecd8edd6353b86d63fd42d5f
JeanetteFernandez <jeanfern555@gmail.com>
2018-12-11 16:41:01 -0600

    Initial Commit - Teacher's will be able to see a list of their students

--- client/src/views/studentListView.js
+++ client/src/views/studentListView.js
@@ -8,24 +8,7 @@ class StudentListView extends Component {
 
   render() {
     return (
-      <div className="container">
-        <div className="flex-container">
-          <div className="block-container" id="studentList">
-            <h1 className="subheader">Students</h1>
-            {this.state.students.map(student => (
-              <div key={student.id} className="bodyText">
-                <NavLink to={`/studentView/${student.id}`}>
-                  <h3>
-                    {student.firstName} {student.lastName}:
-                  </h3>
-                  <h3>{student.instrument}</h3>
-                  <h3>{student.level}</h3>
-                </NavLink>
-              </div>
-            ))}
-          </div>
-        </div>
-      </div>
+     pending
     );
   }
 }

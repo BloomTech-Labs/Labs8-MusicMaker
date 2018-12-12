9851ce86f8b6bece28892c862ad00298b38ead75
JeanetteFernandez <jeanfern555@gmail.com>
2018-12-12 14:35:39 -0600

    Initial Commit -  This page wil allow teacher's to see a list of all their ungraded assignments

--- client/src/views/dashboardAssignmentsView.js
+++ client/src/views/dashboardAssignmentsView.js
@@ -1,63 +1,40 @@
+// Assignment List: This page wil allow teacher's to see a list of all their ungraded assignments
+ 
 import React, { Component } from "react";
-import axios from "axios";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import { Label, Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle, Button } from 'reactstrap';
+import axios from 'axios';
+// import { Route } from "react-router-dom";
+
+// import * as routes from "../constants/routes";
+// import StudentAssignmentsView from "./studentAssignmentsView";
+
 
 class DashboardAssignmentsView extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      assignmentName: "",
-      instructions: "",
-      instrument: "",
-      level: "",
-      piece: "",
-      sheetMusic: ""
+      assignments: []
     };
-  }
+};
 
-  componentDidMount() {
-    axios
-      .get(
-        "https://musicmaker-4b2e8.firebaseapp.com/teacher/pwUGQC7SHBiPKPdnOq2c/assignment/UsvQvEgdUzhpN3iWlcDg"
-      )
+componentDidMount() {
+  const teacherId = 'pwUGQC7SHBiPKPdnOq2c' //this.props.match.params.id;
+
+  axios
+      .get(`https://musicmaker-4b2e8.firebaseapp.com/teacher/${teacherId}/assignments`)
       .then(res => {
-        this.setState({
-          assignmentName: res.data.assignmentName,
-          instructions: res.data.instructions,
-          instrument: res.data.instrument,
-          level: res.data.level,
-          piece: res.data.piece,
-          sheetMusic: res.data.sheetMusic
-        });
+          console.log('Axios*******************', res.data)
+          this.setState({assignments: res.data})
       })
-      .catch(err => console.error("ASSIGNMENTS AXIOS ERROR:", err));
-  }
+      .catch(err => console.error('STUDENT LIST VIEW AXIOS:', err));
+}
+
   render() {
     return (
-      <div className="container">
-        <div className="d-flex">
-          <h1>Assignments View</h1>
-          <Link to="/assignments/create">
-            <h1> + </h1>
-          </Link>
-        </div>
-
-        <div className="d-block">
-          <h5>Test: This is an ungraded assignment for a specific teacher</h5>
-          <p>Assignment Name: {this.state.assignmentName}</p>
-          <p>Piece: {this.state.piece}</p>
-          <p>Instrument: {this.state.instrument}</p>
-          <p>Level: {this.state.level}</p>
-          <p>Sheet Music:<a href={this.state.sheetMusic}><img
-                src={this.state.sheetMusic}
-                alt="pdf image"
-                height="42"
-                width="42"
-              />
-            </a>
-          </p>
-          <p>Instructions:{this.state.instructions}</p>
-        </div>
+      <div>
+        <h1><Label>Assignments</Label></h1>
+       
       </div>
     );
   }

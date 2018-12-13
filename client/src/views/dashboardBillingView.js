f1fae33b55c5faea58caca88a2c3ae3c57f9dbd5
Evan Carlstrom <evan.carlstrom@gmail.com>
2018-12-13 16:27:17 -0500

    added basic container to billing view

--- client/src/views/dashboardBillingView.js
+++ client/src/views/dashboardBillingView.js
@@ -5,11 +5,13 @@ import TakeMoney from "../components/StripeFrontend";
 
 import { Row, Col } from "reactstrap";
 
+const formContainer = { maxWidth: 800, margin: '0 auto 10px', border: "3px solid #A9E8DC" };
+
 class DashboardBillingView extends Component {
   render() {
     return (
       <StripeProvider apiKey="pk_test_YVKiLW0mvujdoxalKItXwyhj">
-        <Row className="container">
+        <Row className="container" style={formContainer}>
           <Col>
             <h1>Billing</h1>
             <Elements>

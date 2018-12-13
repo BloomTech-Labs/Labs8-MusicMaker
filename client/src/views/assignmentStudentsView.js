f30783d9b8ec9bfe2f9a51e097cc8b8a0bdd2604
JeanetteFernandez <jeanfern555@gmail.com>
2018-12-13 09:37:27 -0600

    Modal is working, now I need to modify it so that teacher can assign an assignment.

--- client/src/views/assignmentStudentsView.js
+++ client/src/views/assignmentStudentsView.js
@@ -4,8 +4,9 @@
 //click on student to see student's assignment submission for grading.
 import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
-import { Col, Row, Label, Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle, Button } from 'reactstrap';
+import { Col, Row, Label, Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle } from 'reactstrap';
 import axios from 'axios';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 // import { Route } from "react-router-dom";
 
 // import * as routes from "../constants/routes";
@@ -19,8 +20,17 @@ class StudentAssignmentsView extends Component {
         this.state = {
             // assignments: [],
             students: [],
+            modal:false
         };
+
+        this.toggle = this.toggle.bind(this);
     };
+
+    toggle() {
+        this.setState({
+          modal: !this.state.modal
+        });
+      }
 
     // componentDidMount() {
     //     const teacherId = 'pwUGQC7SHBiPKPdnOq2c' //this.props.match.params.id;
@@ -64,11 +74,21 @@ class StudentAssignmentsView extends Component {
         return(
             <div>
                 <h1><Label>Student's Assigned to the Assignment</Label></h1>
-                <h1>+</h1>
+                <Button onClick={this.toggle}>+</Button>
+                <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
+                <ModalHeader toggle={this.toggle}>Assign Assignment to Student</ModalHeader>
+                <ModalBody>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </ModalBody>
+                <ModalFooter>
+                    <Button color="primary" onClick={this.toggle}>Do Something</Button>{' '}
+                    <Button color="secondary" onClick={this.toggle}>Cancel</Button>
+                </ModalFooter>
+                </Modal>
             <div>
                 {this.state.students.map(student => (
                     <Row key={student[2]} style={{border:"1px solid black"}}>
-                        <NavLink to={`grading/${student[2]}`} style={{textDecoration:"none"}} >
+                        <NavLink to={`/grading/${student[2]}`} style={{textDecoration:"none"}} >
                             <Col>{student[3]} {student[4]}</Col> {/*student's name*/}
                         </NavLink>
                             <Col>{student[5]}</Col> {/*student's assignment due date*/}

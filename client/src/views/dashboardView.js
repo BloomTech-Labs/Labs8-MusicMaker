9b3ddd415ba0ff8505cb10013f6280563341e33a
Evan Carlstrom <evan.carlstrom@gmail.com>
2018-12-14 11:52:42 -0500

    added padding to dashboard view and updated the + button style on that page

--- client/src/views/dashboardView.js
+++ client/src/views/dashboardView.js
@@ -5,6 +5,7 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import firebase from 'firebase';
+import { Button } from 'reactstrap';
 
 const formContainer = { maxWidth: 800, margin: '0 auto 10px', border: "3px solid #A9E8DC" };
 
@@ -41,11 +42,11 @@ class DashboardView extends Component {
         <div className="d-flex ">
           <h1 style={{marginTop: "10px", marginBottom: "10px"}}>Dashboard</h1>
           <Link to="/assignments/create">
-            <h1 style={{position: "relative", left: "30px", top: "8px"}}> + </h1>
+            <Button style={{position: "relative", left: "30px", top: "20px"}}> + </Button>
           </Link>
         </div>
         <div>
-          <h3 style={{paddingTop: "20px", paddingBottom: "20px"}}>QR Code</h3>
+          <h3 style={{padding: "5px 5px 15px 5px"}}>QR Code:</h3>
           <a href={this.state.qrcode}>
             <img
               src={this.state.qrcode}
